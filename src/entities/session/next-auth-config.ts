9676d805adf5eb5710a4994a292a13c62a723063
adm <vkolosov1@mail.ru>
2025-03-26 19:51:55 +0300

    fixed scheme

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -3,18 +3,25 @@ import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { dbClient } from "@/src/shared/lib/db"
 import { privateConfig } from "@/src/shared/config/private"
-import {compact} from 'lodash-es'
+import { compact } from 'lodash-es'
 
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient),
   providers: compact([
     privateConfig.GITHUB_ID &&
-        privateConfig.GITHUB_SECRET &&
-            GithubProvider({
-              clientId: privateConfig.GITHUB_ID,
-              clientSecret: privateConfig.GITHUB_SECRET,
-            }),
+      privateConfig.GITHUB_SECRET &&
+      GithubProvider({
+        clientId: privateConfig.GITHUB_ID,
+        clientSecret: privateConfig.GITHUB_SECRET,
+      }),
   ]),
+  secret: privateConfig.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt"
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
 }
 
 export default nextAuthConfig
