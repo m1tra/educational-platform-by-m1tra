1aaf7c69ff5efd53f7464d05f1b5bc911b02d475
adm <vkolosov1@mail.ru>
2025-04-09 18:01:34 +0300

    fixed build error

--- src/app/(public)/custom-test/page.tsx
+++ src/app/(public)/custom-test/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { Wrapper } from "@/src/shared/components/custom-tasks/wrapper";
+import { BarLoader } from "@/src/shared/components/ui/loader";
+import { Suspense } from "react";
 
 
 export default function EditPage(){
@@ -7,7 +9,9 @@ export default function EditPage(){
         <div className="flex min-h-screen flex-col">
             <section className="flex-1 bg-background flex justify-center">
                 <div className="container py-6 md:py-10">
-                  <Wrapper />
+                <Suspense fallback={<BarLoader/>}>
+                    <Wrapper />
+                </Suspense>
                 </div>
             </section>
         </div>
